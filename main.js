c5eefd06b87f4d76f748ca74119c64247c3a85db
Chakit Sharma <chakitsharma444@outlook.com>
2023-08-26 22:37:26 +0530

    Testing Mac disabled.

--- main.js
+++ main.js
@@ -128,28 +128,42 @@ window.addEventListener("load", function () {
   });
 });
 
-document.addEventListener('contextmenu', function(e) {
+document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
 });
 
-document.onkeydown = function(e) {
+document.onkeydown = function (e) {
   if (e.keyCode === 123) {
-      return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) {
-      return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) {
-      return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) {
-      return false;
-  }
-  if (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) {
-      return false;
-  }
-  if(e.ctrlKey && e.keyCode==='S'.charCodeAt(0)){
     return false;
   }
-}
-
+  if (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode === "C".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode === "S".charCodeAt(0)) {
+    return false;
+  }
+  if (e.metaKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.metaKey && e.shiftKey && e.keyCode === "C".charCodeAt(0)) {
+    return false;
+  }
+  if (e.metaKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.metaKey && e.keyCode === "U".charCodeAt(0)) {
+    return false;
+  }
+  if (e.metaKey && e.keyCode === "S".charCodeAt(0)) {
+    return false;
+  }
+};
